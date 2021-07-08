6ce1acb8f78fb2b3885ba772d367a580175bec54
Lê Nguyễn Thành Nhân <74607205+lenhannhan123@users.noreply.github.com>
2021-07-08 13:27:51 +0700

    update home admin

--- resources/views/layoutAdmin/nav-bar.blade.php
+++ resources/views/layoutAdmin/nav-bar.blade.php
@@ -1,10 +1,37 @@
-<nav class="main-head navbar navbar-expand navbar-white navbar-light">
-    <!--Left navbar links-->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
     <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-        </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="{{url('admin')}}" class="nav-link">Home</a>
+      </li>
     </ul>
+
     <!-- SEARCH FORM -->
-    
-</nav>
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fas fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+
+    <!-- Right navbar links -->
+   
+
+    <ul class="navbar-nav ml-auto" style="width:auto">
+      <li class="nav-item">Xin chào! Lê Nguyễn Thành Nhân</li>
+  
+      <li class="nav-item">
+        <a class="nav-link" href="{{url('logout')}}">
+          <input type="button" class="btn btn-success" value="Log Out">
+        </a>
+      </li>
+    </ul>
+  </nav>
