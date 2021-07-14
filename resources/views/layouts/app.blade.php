8d3e84c445c9a887217b5e306fec8eaffa3103e9
Thien Nguyen <j.thien1551@gmail.com>
2021-07-14 21:03:46 +0700

    unfuk#1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,27 +45,28 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Đăng nhập') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Đăng ký') }}</a>
                                 </li>
                             @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <img src="images/avatar.png" style="width: 40px" class="rounded-circle">
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    <a class="dropdown-item" id="dropdown-list" href="#">Trang cá nhân</a>
+                                    <a class="dropdown-item" id="dropdown-list" href="#">Xe của tôi</a>
+                                    <a class="dropdown-item" id="dropdown-list" href="#">Chuyến của tôi</a>
+                                    <a class="dropdown-item" id="dropdown-list" href="#">Đổi mật khẩu</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Đăng xuất') }}</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
