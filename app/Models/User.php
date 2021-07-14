9654942bfec08cd1e86fb9f24ddbf2b7c516f7a5
Thien Nguyen <j.thien1551@gmail.com>
2021-07-14 16:31:07 +0700

    Fix bug thanks to Nhan

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+    public $table = 'tb_user';
+    public $primaryKey = 'user_id';
+
     protected $fillable = [
         'name',
         'email',
