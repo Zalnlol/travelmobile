00d18722dcf74c25b6170fe328b4a7afb7f114b1
Thien Nguyen <j.thien1551@gmail.com>
2021-07-23 14:06:00 +0700

    re route blog cho Toàn

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,22 +3,23 @@
 
 @section('bodycode')
 
-
-<div class="row">
-    @foreach($data as $row)
-    <div class="col-md-6">
-        
-        
-        <img src="{{asset("img/blog/$row->blog_pic")}}" alt=""
-        style="width:100%">
-        
-        <h3>{{$row->title}}</h3>
-        <p>{{$row->content}}</p>
-        
+<div class="container">
+    <div class="row">
+        @foreach($data as $row)
+        <div class="col-md-6">
+            
+            
+            <img src="{{asset("img/blog/$row->blog_pic")}}" alt=""
+            style="width:100%">
+            
+            <h3>{{$row->title}}</h3>
+            <p>{{$row->content}}</p>
+            
+        </div>
+    
+        @endforeach
     </div>
-
-    @endforeach
-    </div>
+</div>
     
  @endsection
 
