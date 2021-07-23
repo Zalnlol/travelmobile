395877f64694b50de3df3bcd3c0bc021cacb7921
phamquoctoan123 <72727724+phamquoctoan123@users.noreply.github.com>
2021-07-23 13:57:15 +0700

    Blog front end
    23/7/2021

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,27 @@
+@extends('layoutUser.layout')
+@section('titleweb', 'Blog')
+
+@section('bodycode')
+
+
+<div class="row">
+    @foreach($data as $row)
+    <div class="col-md-6">
+        
+        
+        <img src="{{asset("img/blog/$row->blog_pic")}}" alt=""
+        style="width:100%">
+        
+        <h3>{{$row->title}}</h3>
+        <p>{{$row->content}}</p>
+        
+    </div>
+
+    @endforeach
+    </div>
+    
+ @endsection
+
+
+
+
