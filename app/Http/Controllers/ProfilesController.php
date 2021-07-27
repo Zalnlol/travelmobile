dc0ede60c901fb26ca9e8f373e8b2ed9460c9e8d
Thien Nguyen <j.thien1551@gmail.com>
2021-07-27 17:55:05 +0700

    route xem profile user khac

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -32,10 +32,8 @@ class ProfilesController extends Controller
 
         return view('profiles.index', compact('user','user_id'));  
         }
-        
-    
-    
 
+        
     public function edit(Request  $request ,User $user)
     {   
 
@@ -44,46 +42,46 @@ class ProfilesController extends Controller
         return view('profiles.edit', compact('user', 'user_id'));
     }
 
-    //WIP
-    // public function update(Request  $request ,User $user)
-    // {   
-    //     $name = $request->input('name');
-    //     $email = $request->input('email');
-    //     $mobile = $request->input('mobile');
-    //     $gender = $request->input('gender');
-    //     $status = $request->input('status');
-    //     $driver_id = $request->input('driver_id');
-    //     $data = request()->validate([
-    //         'name' => ['required', 'string', 'max:255'],
-    //         'image' => '',
-    //         'mobile' => 'required',
-    //         'dob' => '',
-    //         'gender' => 'required',
-    //         'email' => ['required', 'string', 'email', 'max:255', 'unique:tb_user'],
-    //         'driver_id' => '',
-    //         'driver_id_image' => '',
-    //     ]);
-    //     $user_id = $request->session()->get('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d');
-    //     // dd($user_id);
-    //     //Xử lý upload hình vào thư mục
-    //     if($request->hasFile('image')){
-    //         $file=$request->file('image');
-    //         $extension = $file->getClientOriginalExtension();
-    //         if($extension != 'jpg' && $extension != 'png' && $extension != 'jpeg'){
-    //             return redirect('user/update')->width('Lỗi', ' Bạn chỉ được chọn file có đuôi jpg, png, jpeg');
-    //         }
-    //         $imageName = $file->getClientOriginalName();
-    //         $file->move("public/images",$imageName);
-    //     } else {//Không upload hình mới => giữ lại hình cũ
-    //         $user = DB::table('tb_user')
-    //                 ->where('user_id',intval($id))
-    //                 ->first();
-    //                 $imageName = $user->driver_id_image;
-    //     }
-    //     $user = DB::table('tb_user')
-    //         ->where('user_id',intval($id))
-    //         ->update(['name'=>$name, 'email'=>$email, 'mobile'=>$mobile, 'gender'=>$gender, 'status'=>$status, 'driver_id'=>$driver_id, 'driver_id_image'=>$imageName]);
-    //         return redirect()->action([AccountController::class, 'index']);
-    //     return redirect('profiles.index', compact('user','user_id'));
-    // }
+    // WIP
+    public function update(Request  $request ,User $user)
+    {   
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $mobile = $request->input('mobile');
+        $gender = $request->input('gender');
+        $status = $request->input('status');
+        $driver_id = $request->input('driver_id');
+        $data = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'image' => '',
+            'mobile' => 'required',
+            'dob' => '',
+            'gender' => 'required',
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:tb_user'],
+            'driver_id' => '',
+            'driver_id_image' => '',
+        ]);
+        $user_id = $request->session()->get('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d');
+        // dd($user_id);
+        //Xử lý upload hình vào thư mục
+        if($request->hasFile('image')){
+            $file=$request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            if($extension != 'jpg' && $extension != 'png' && $extension != 'jpeg'){
+                return redirect()->action([AccountController::class, 'edit'])->width('Lỗi', ' Bạn chỉ được chọn file có đuôi jpg, png, jpeg');
+            }
+            $imageName = $file->getClientOriginalName();
+            $file->move("public/images",$imageName);
+        } else {//Không upload hình mới => giữ lại hình cũ
+            $user = DB::table('tb_user')
+                    ->where('user_id',intval($user_id))
+                    ->first();
+                    $imageName = $user->driver_id_image;
+        }
+        $user = DB::table('tb_user')
+            ->where('user_id',intval($user_id))
+            ->update(['name'=>$name, 'email'=>$email, 'mobile'=>$mobile, 'gender'=>$gender, 'status'=>$status, 'driver_id'=>$driver_id, 'driver_id_image'=>$imageName]);
+            return redirect()->action([AccountController::class, 'index']);
+        return redirect('profiles.index', compact('user','user_id'));
+    }
 }
