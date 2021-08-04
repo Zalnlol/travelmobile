59db7598c6db7a82342eb97fdd5644ba6c31d05e
phamquoctoan123 <72727724+phamquoctoan123@users.noreply.github.com>
2021-08-04 22:22:59 +0700

    update
    4/8/2021

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,25 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BlogController extends Controller
 {
+
     //Mở trang blog
     public function blog()
     {
-        $data = Blog::OrderBy("created_at","DESC")->paginate(6);
-        // $data = Blog::;
+
+        $data = Blog::OrderBy("created_at", "DESC")->paginate(6);
+
         return view("blog", compact("data"));
     }
 
     public function get()
     {
-        
+
         $ds = Blog::paginate(6);
-        
+
         return view('Admin-Blog.blog-index', compact('ds'));
     }
-
 
     //ham tao
     public function createBlog()
@@ -42,7 +44,7 @@ class BlogController extends Controller
                 return redirect('admin/blog/create')->with('loi', 'Bạn chỉ được chọn file có đuôi jpg,png,jpeg');
             }
             $imageName = $file->getClientOriginalName();
-            $file->move("img/blog", $imageName);
+            $file->move("img/Blog", $imageName);
         } else {
             $imageName = null;
         }
@@ -53,7 +55,6 @@ class BlogController extends Controller
         return redirect("/admin/blog");
     }
 
-
     //ham xoa
     public function deleteBlog($blog_id)
     {
@@ -62,17 +63,14 @@ class BlogController extends Controller
         return redirect("/admin/blog");
     }
 
-
     //ham cap nhat
     public function editBlog($blog_id)
     {
         $blog = Blog::find($blog_id);
 
-
         return view('Admin-Blog.blog-edit', compact('blog'));
     }
 
-     
     public function editPostBlog(Request $request)
     {
         $blog = $request->all();
@@ -85,7 +83,40 @@ class BlogController extends Controller
                 return redirect('admin/blog/editBlog')->with('loi', 'Bạn chỉ được chọn file có đuôi jpg,png,jpeg');
             }
             $imageName = $file->getClientOriginalName();
-            $file->move("img/blog", $imageName);
+            $file->move("img/Blog", $imageName);
+        } else {
+            $b = Blog::find($request->blog_id);
+            $imageName = $b->blog_pic;
+        }
+        $b = new Blog($blog);
+
+        $b->blog_pic = $imageName;
+        // dd($b);
+        $b->exists = true;
+        $b->save(); //insert $b vo bang tbbook
+
+        return redirect('/admin/blog'); //quay ve trang book index
+    }
+
+    public function DetailBlog($blog_id)
+    {
+        $blog = DB::table('tb_blog')->find($blog_id);
+        return view("Admin-Blog.blog-view", compact('blog'));
+    }
+
+    public function DetailPostLog(Request $request)
+    {
+        $blog = $request->all();
+
+        $imageName = '';
+        if ($request->hasFile('blog_pic')) {
+            $file = $request->file('blog_pic');
+            $extension = $file->getClientOriginalExtension();
+            if ($extension != 'jpg' && $extension != 'png' && $extension != 'jpeg') {
+                return redirect('admin/blog/DetailsBlog')->with('loi', 'Bạn chỉ được chọn file có đuôi jpg,png,jpeg');
+            }
+            $imageName = $file->getClientOriginalName();
+            $file->move("img/Blog", $imageName);
         } else {
             $b = Blog::find($request->blog_id);
             $imageName = $b->blog_pic;
