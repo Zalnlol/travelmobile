cb8d300a376d2e4ef1842aab3546c73557bd3828
Thien Nguyen <j.thien1551@gmail.com>
2021-08-05 22:27:18 +0700

    missing $request in accountcontroller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -11,7 +11,7 @@ use App\Models\User;
 class AccountController extends Controller
 {   
     //tra ve view listing danh sach users
-    public function index(){
+    public function index(Request $request){
         $ds = User::paginate(10);
         $currentuser_id = $request->session()->get('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d');
         return view("Admin-User.index",compact('ds','currentuser_id'));
